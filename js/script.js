5dc6f33f0203dabb63f2103633d3b4c4430a5ac2
IsmailBayramov <geri60884@gmail.com>
2023-11-21 21:59:27 +0500

    main

--- js/script.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-  $(".slider").slick({
-    autoplay: true,
-    autoplaySpeed: 4000,
-    arrows: true,
-    slidesToScroll: 1,
-  });
-};
--- newCustomer.php
+++
@@ -1,8 +0,0 @@
-<?php
-require './block/tgNotify.php';
-
-sendTelegramMessage("Клиент вызывает такси\nНомер:" .  $_POST['phone'] . "\n Адрес:" .  $_POST['address']);
-header("Location: ./". $_POST['page']. ".php?d=0");
-
-
-?>
